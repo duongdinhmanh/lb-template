1a475a78953560764d9d915d68013beb9be6664e
duong dinh manh <38414583+duongdinhmanh@users.noreply.github.com>
2018-12-11 10:14:14 +0700

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,18 @@
+{
+  "name": "lb-tempalte-bower",
+  "version": "1.0.1",
+  "authors": [
+    "Duong DInh Manh <duongdinhmanh252@gmail.com>"
+  ],
+  "description": "Framgia standard lb-template Bower",
+  "main": "/",
+  "keywords": [
+    "fonticons"
+  ],
+  "license": "MIT",
+  "homepage": "https://github.com/duongdinhmanh/lb-template.git",
+  "ignore": [
+    "README.md"
+  ]
+}
+ 
